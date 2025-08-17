ac78d785c1ea71e0dfac6c52d0b568952e8e3a64
Rannya7x <rannya7x@gmail.com>
2025-08-17 10:38:39 -0400

    Add linked list implementation with basic operations

---
+++ Aula 3/usalse.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "listase.h"
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    t_lse* deNumeros = criar_lse();
+    scanf("%d", &num);
+    while(num!=-1){
+        //inserir_lse(deNumeros, num);
+        inserir_final_lse(deNumeros, num);
+        scanf("%d", &num);
+    }
+
+    imprimir_lse(deNumeros);
+    return 0;
+}
